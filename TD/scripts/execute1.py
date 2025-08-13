ac9bdcd87f89b5e29d28e4df595d1124d5bcf1ea
Matt Ross <mattross1080@gmail.com>
2025-08-13 15:10:38 -0400

    Adds simpler startup and pars

--- TD/scripts/execute1.py
+++ TD/scripts/execute1.py
@@ -5,12 +5,9 @@ except ModuleNotFoundError:
     from vvox_tdtools.td_mock import op, monitors, root  #pylint: disable=ungrouped-imports
 
 def onStart():
-    is_production = root.var('mode') == 'production'
-    if is_production:
-          print("LAUNCHING MAIN WINDOW")
-          op('main').par.winopen.pulse()
-    else:
-          print("NOT LAUNCHING MAIN WINDOW - DEV MODE")
+
+    op('main').par.winopen.pulse()
+
     return
 
 def onCreate():
