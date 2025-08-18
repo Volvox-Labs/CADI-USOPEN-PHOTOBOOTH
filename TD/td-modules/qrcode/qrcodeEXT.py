7149a1486a745fa9223cd7976f7b071170c649f9
Matt Ross <mattross1080@gmail.com>
2025-08-18 17:41:44 -0400

    Updates failure from uploader

--- TD/td-modules/qrcode/qrcodeEXT.py
+++ TD/td-modules/qrcode/qrcodeEXT.py
@@ -47,7 +47,7 @@ class QrcodeEXT(PhotoboothSceneEXT):
             self.Me.par.Showqrcode = 1
         else:
             print("Upload not complete, skipping loading bar pulse.")
-            self.Me.op("timeout_timer").par.Start.pulse()
+            self.Me.op("timeout_timer").par.start.pulse()
         
         op.fade_control.par.Fadein.pulse()
         super()._onEnterscene()
